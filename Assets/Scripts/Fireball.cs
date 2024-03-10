f48470cfb1771f55ede9e0b9642f1d502ea0cc7e
tea1331 <158470194+tea1331@users.noreply.github.com>
2024-03-10 19:02:20 +0300

    myproject

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
+}
